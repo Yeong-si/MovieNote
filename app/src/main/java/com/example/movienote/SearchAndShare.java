76eee454ce03aa8de5c0265bab0fa1609e11cc4b
Chanmin-Hwang <amazon1816@gmail.com>
2023-11-14 16:09:46 +0900

    SearchAndShare 클래스 추가

---
+++ app/src/main/java/com/example/movienote/SearchAndShare.java
@@ -0,0 +1,14 @@
+package com.example.movienote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchAndShare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_and_share);
+    }
+}
