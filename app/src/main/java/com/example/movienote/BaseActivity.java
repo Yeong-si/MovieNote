c27cc0488ccbc4b70f4d13c777d6bc2b2edbb8d7
Chanmin-Hwang <amazon1816@gmail.com>
2023-11-15 17:40:21 +0900

    .

--- app/src/main/java/com/example/movienote/BaseActivity.java
+++ app/src/main/java/com/example/movienote/BaseActivity.java
@@ -2,11 +2,13 @@ package com.example.movienote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.movienote.databinding.ActivityBaseBinding;
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityBaseBinding binding;
     @Override
@@ -14,5 +16,23 @@ public class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityBaseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.MovieSearchButton.setOnClickListener(this);
+        binding.PartySearchButton.setOnClickListener(this);
+        binding.PartyInformationButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == binding.MovieSearchButton){
+            Intent intent = new Intent(getApplicationContext(), MovieSearchActivity.class);
+            startActivity(intent);
+        } else if (v == binding.PartySearchButton) {
+            Intent intent = new Intent(getApplicationContext(), PartySearchActivity.class);
+            startActivity(intent);
+        } else if (v == binding.PartyInformationButton) {
+            Intent intent = new Intent(getApplicationContext(), PartyInformationActivity.class);
+            startActivity(intent);
+        }
     }
 }
