6d5fbb3d1be4074b938cb87d63914f6670421565
dtd1110@naver.com <dtd1110@gmail.com>
2023-11-23 14:26:37 +0900

    .

--- app/src/main/java/com/example/movienote/Movie.java
+++ app/src/main/java/com/example/movienote/Movie.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
@@ -13,13 +14,21 @@ import java.io.IOException;
 
 public class Movie extends Fragment {
 
-    public static void main(String input ){
+    public static void movieGet(String input )throws IOException{
 
-        StringBuilder urlBuilder = new StringBuilder("http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&nation=대한민국");/*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=서비스키");/*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("val001","UTF-8") + "=" + URLEncoder.encode("2018", "UTF-8"));/*상영년도*/
-        urlBuilder.append("&" + URLEncoder.encode("val002","UTF-8") + "=" + URLEncoder.encode("01", "UTF-8"));/*상영 월*/
-        URL url = new URL(urlBuilder.toString());
+        //StringBuilder urlBuilder = new StringBuilder("http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&nation=대한민국");/*URL*/
+        StringBuilder urlBuilder = new StringBuilder("http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=");
+       // urlBuilder.append("?" + URLEncoder.encode("V40N5WM77MESM46PM90Y","UTF-8") + "=서비스키");/*Service Key*/
+        urlBuilder.append(URLEncoder.encode("V40N5WM77MESM46PM90Y","UTF-8") + "=서비스키");/*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("query","UTF-8") + "=" + URLEncoder.encode(input, "UTF-8"));
+        //urlBuilder.append("&" + URLEncoder.encode("val001","UTF-8") + "=" + URLEncoder.encode("2018", "UTF-8"));/*상영년도*/
+        //urlBuilder.append("&" + URLEncoder.encode("val002","UTF-8") + "=" + URLEncoder.encode("01", "UTF-8"));/*상영 월*/
+        URL url = null;
+        try {
+            url = new URL(urlBuilder.toString());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -34,6 +43,13 @@ public class Movie extends Fragment {
         else {
             rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
-    }
 
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line); }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+    }
 }
