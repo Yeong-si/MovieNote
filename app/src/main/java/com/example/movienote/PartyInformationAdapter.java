e7223675abc8cc039e1f73b75197f8bc08965c36
황찬민 <amazon1816@gmail.com>
2023-11-30 00:28:01 +0900

    recyclerview

--- app/src/main/java/com/example/movienote/PartyInformationAdapter.java
+++ app/src/main/java/com/example/movienote/PartyInformationAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -33,23 +34,45 @@ public class PartyInformationAdapter extends RecyclerView.Adapter<PartyInformati
     @Override
     public void onBindViewHolder(@NonNull PartyInformationAdapter.PartyInformationViewHolder holder, int position) {
 
-        Party party partyArrayList.get(position);
+        Party party = partyArrayList.get(position);
 
-        holder.
+        holder.member_size.setText(party.getMember().size());
+        holder.accountHolderName.setText(party.getAccountHolderName());
+        holder.price.setText(party.getPrice().toString());
+        holder.subscription.setText(party.getSubscription());
+        holder.id.setText(party.getId());
+        holder.password.setText(party.getPassword());
+        holder.member1.setText(party.getMember().get(1));
+        holder.member2.setText(party.getMember().get(2));
+        holder.member3.setText(party.getMember().get(3));
+        holder.member4.setText(party.getMember().get(4));
+        holder.accountNumber.setText(party.getAccountNumber().toString());
+        holder.bankName.setText(party.getBankName());
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
-    }
+    public int getItemCount() {return partyArrayList.size();}
 
     public static class PartyInformationViewHolder extends RecyclerView.ViewHolder {
 
-        TextView subscription,member_size,price;
+        TextView subscription,member_size,price,id,password,accountHolderName,accountNumber,member1,member2,member3,member4,bankName;
+        ImageView ott_icon;
         public PartyInformationViewHolder(@NonNull View itemView) {
 
             super(itemView);
+            subscription = itemView.findViewById(R.id.subscription);
+            member_size = itemView.findViewById(R.id.member_size);
+            price = itemView.findViewById(R.id.price);
+            id = itemView.findViewById(R.id.id);
+            password = itemView.findViewById(R.id.password);
+            accountHolderName = itemView.findViewById(R.id.accountHolderName);
+            accountNumber = itemView.findViewById(R.id.accountNumber);
+            member1 = itemView.findViewById(R.id.member1);
+            member2 = itemView.findViewById(R.id.member2);
+            member3 = itemView.findViewById(R.id.member3);
+            member4 = itemView.findViewById(R.id.member4);
 
+            ott_icon = itemView.findViewById(R.id.ott_icon);
         }
     }
 }
