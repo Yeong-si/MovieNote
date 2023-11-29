cc1e10e4f3a9bc2f9525c4e334eb3ca681675e32
황찬민 <amazon1816@gmail.com>
2023-11-29 14:23:15 +0900

    .

--- app/src/main/java/com/example/movienote/PartySearchActivity.java
+++ app/src/main/java/com/example/movienote/PartySearchActivity.java
@@ -2,13 +2,32 @@
 
 package com.example.movienote;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.movienote.databinding.ActivityPartySearchBinding;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class PartySearchActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<Party> partyArrayList;
+    PartySearchAdapter partySearchAdapter;
+    FirebaseFirestore db;
+    ProgressDialog progressDialog;
 
     ActivityPartySearchBinding binding;
     @Override
@@ -17,5 +36,54 @@ public class PartySearchActivity extends AppCompatActivity {
 
         binding = ActivityPartySearchBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching Data...");
+        progressDialog.show();
+
+        recyclerView = binding.partySearchRecyclerView;
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db = FirebaseFirestore.getInstance();
+        partyArrayList = new ArrayList<Party>();
+        partySearchAdapter = new PartySearchAdapter(PartySearchActivity.this,partyArrayList);
+
+        recyclerView.setAdapter(partySearchAdapter);
+
+        EventChangeListener();
+    }
+
+    private void EventChangeListener() {
+
+        db.collection("Party").orderBy("subscription", Query.Direction.ASCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        if (error != null) {
+
+                            if (progressDialog.isShowing()) {
+                                progressDialog.dismiss();
+                            }
+
+                            Log.e("FireStore error",error.getMessage());
+                            return;
+                        }
+
+                        for(DocumentChange dc: value.getDocumentChanges()) {
+
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+                                partyArrayList.add(dc.getDocument().toObject(Party.class));
+                            }
+
+                            partySearchAdapter.notifyDataSetChanged();
+                            if (progressDialog.isShowing()) {
+                                progressDialog.dismiss();
+                            }
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/movienote/PartySearchAdapter.java
@@ -0,0 +1,59 @@
+package com.example.movienote;
+
+import android.content.Context;
+import android.provider.Telephony;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PartySearchAdapter extends RecyclerView.Adapter<PartySearchAdapter.PartySearchViewHolder> {
+
+    Context context;
+    ArrayList<Party> partyArrayList;
+
+    public PartySearchAdapter(Context context,ArrayList<Party> partyArrayList) {
+        this.context = context;
+        this.partyArrayList = partyArrayList;
+    }
+
+    @NonNull
+    @Override
+    public PartySearchAdapter.PartySearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.item_party,parent,false);
+
+        return new PartySearchViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PartySearchAdapter.PartySearchViewHolder holder, int position) {
+
+        Party party = partyArrayList.get(position);
+
+        holder.title.setText(party.getTitle());
+        holder.member_size.setText(party.getMember().size());
+        holder.price.setText(party.getPrice().toString());
+        holder.subscription.setText(party.getSubscription());
+    }
+
+    @Override
+    public int getItemCount() {return partyArrayList.size();}
+
+    public static class PartySearchViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title,price,member_size,subscription;
+        public PartySearchViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            price = itemView.findViewById(R.id.price);
+            member_size = itemView.findViewById(R.id.member_size);
+            subscription = itemView.findViewById(R.id.subscription);
+        }
+    }
+}
