28cd7b98111f69e76990d34d0dc4a6a6769db2ba
황찬민 <amazon1816@gmail.com>
2023-12-07 16:41:40 +0900

    .

--- app/src/main/java/com/example/movienote/MovieNoteSearchActivity.java
+++ app/src/main/java/com/example/movienote/MovieNoteSearchActivity.java
@@ -102,62 +102,62 @@ public class MovieNoteSearchActivity extends AppCompatActivity {
 //        EventChangeListener();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // below line is to get our inflater
-        MenuInflater inflater = getMenuInflater();
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // below line is to get our inflater
+//        MenuInflater inflater = getMenuInflater();
+//
+//        // inside inflater we are inflating our menu file.
+//        inflater.inflate(R.menu.search_menu, menu);
+//
+//        // below line is to get our menu item.
+//        MenuItem searchItem = menu.findItem(R.id.actionSearch);
+//
+//        // getting search view of our item.
+//        SearchView searchView = (SearchView) searchItem.getActionView();
+//
+//        // below line is to call set on query text listener method.
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                // inside on query text change method we are
+//                // calling a method to filter our recycler view.
+//                filter(newText);
+//                return false;
+//            }
+//        });
+//        return true;
+//    }
 
-        // inside inflater we are inflating our menu file.
-        inflater.inflate(R.menu.search_menu, menu);
 
-        // below line is to get our menu item.
-        MenuItem searchItem = menu.findItem(R.id.actionSearch);
-
-        // getting search view of our item.
-        SearchView searchView = (SearchView) searchItem.getActionView();
-
-        // below line is to call set on query text listener method.
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // inside on query text change method we are
-                // calling a method to filter our recycler view.
-                filter(newText);
-                return false;
-            }
-        });
-        return true;
-    }
-
-
-    private void filter(String text) {
-        // creating a new array list to filter our data.
-        ArrayList<Note> filteredlist = new ArrayList<Note>();
-
-        // running a for loop to compare elements.
-        for (Note item : noteArrayList) {
-            // checking if the entered string matched with any item of our recycler view.
-            if (item.getNote().toLowerCase().contains(text.toLowerCase())) {
-                // if the item is matched we are
-                // adding it to our filtered list.
-                filteredlist.add(item);
-            }
-        }
-        if (filteredlist.isEmpty()) {
-            // if no item is added in filtered list we are
-            // displaying a toast message as no data found.
-            Toast.makeText(this, "No Data Found..", Toast.LENGTH_SHORT).show();
-        } else {
-            // at last we are passing that filtered
-            // list to our adapter class.
-            movieNoteAdapter.filterList(filteredlist);
-        }
-    }
+//    private void filter(String text) {
+//        // creating a new array list to filter our data.
+//        ArrayList<Note> filteredlist = new ArrayList<Note>();
+//
+//        // running a for loop to compare elements.
+//        for (Note item : noteArrayList) {
+//            // checking if the entered string matched with any item of our recycler view.
+//            if (item.getNote().toLowerCase().contains(text.toLowerCase())) {
+//                // if the item is matched we are
+//                // adding it to our filtered list.
+//                filteredlist.add(item);
+//            }
+//        }
+//        if (filteredlist.isEmpty()) {
+//            // if no item is added in filtered list we are
+//            // displaying a toast message as no data found.
+//            Toast.makeText(this, "No Data Found..", Toast.LENGTH_SHORT).show();
+//        } else {
+//            // at last we are passing that filtered
+//            // list to our adapter class.
+//            movieNoteAdapter.filterList(filteredlist);
+//        }
+//    }
 
 //    private void EventChangeListener() {
 //
